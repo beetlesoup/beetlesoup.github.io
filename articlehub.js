f74ab5b74d1f6773ea53c04236e78bd49adb05c9
Betule Sairafi <sairafi.b@gmail.com>
2022-11-06 11:06:02 +0300

    well

--- articlehub.js
+++ articlehub.js
@@ -1,42 +1,44 @@
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+// If we save our current state somewhere, we can easily filter the divs.
+var checkedCategories = ["cars", "animals", "fruits", "colors"];
+// We need a function that detects the click on a checkbox and adds/removes that category.
+var changeCategory = function changeCategory(event) {
+  // The event object will tell us exactly what was clicked.
+  var checkbox = event.target;
+  // The category we want toa dd or remove is the attribute
+  var category = checkbox.getAttribute("data-category");
+  // To check if we already have the category in the array, we just check the index.
+  var savedCategoryIndex = checkedCategories.indexOf(category);
+  // If the checkbox is checked, that category has to already exist in the array or get added.
+  if (checkbox.checked && savedCategoryIndex === -1) {
+    checkedCategories.push(category);
   }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  // if it is not checked and is present in the array, it needs to be removed.
+  else if (!checkbox.checked && savedCategoryIndex !== -1) {
+    checkedCategories.splice(savedCategoryIndex, 1);
   }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+  renderCategories();
+};
+// We want a reusable function that will show/hide any categories we want to see.
+var renderCategories = function renderCategories() {
+  // We need a list of all the divs. So we just select all the divs that have the data-category attribute and slice them into an array.
+  // Could be replaced by Array.from() if your browser supports it.
+  var categoryDivs = Array.prototype.slice.call(document.querySelectorAll("div[data-category]"));
+  // Now we need to loop over all the divs and check if they need to get hidden or not.
+  categoryDivs.forEach(function(div) {
+    // Get all the tags the div has
+    var tags = div.getAttribute("data-category").split(" ");
+    // If at least one tag of the div is inside our categories array, we know it'll need to get shown.
+    var divShouldBeShown = tags.some(function(tag) {
+      return checkedCategories.indexOf(tag) !== -1;
+    });
+    // The decide if we need to hide the div or not.
+    // Can be replaced by a classList.toggle() if your browser supports it.
+    if (divShouldBeShown && div.className.indexOf("hidden") !== -1) {
+      div.className = div.className.replace("hidden", "");
+    } else if (!divShouldBeShown && div.className.indexOf("hidden") === -1) {
+      div.className = div.className + " hidden";
     }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
   });
-}
+};
+// Finally we have to add an event to the checkboxes.
+document.querySelector("#categoryBoxes").addEventListener('click', changeCategory);
