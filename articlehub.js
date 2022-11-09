9ef1f77133013679d7d09c0586eb85b3ec497d27
Betule Sairafi <sairafi.b@gmail.com>
2022-11-09 07:23:21 +0300

    update

--- articlehub.js
+++ articlehub.js
@@ -42,3 +42,19 @@ var renderCategories = function renderCategories() {
 };
 // Finally we have to add an event to the checkboxes.
 document.querySelector("#categoryBoxes").addEventListener('click', changeCategory);
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
